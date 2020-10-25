88b842f9fb6c70008a9916ea2ca77a3c69ce7843
Nick Johnson <nicklynnjohnson@gmail.com>
2020-10-24 21:12:20 -0700

    Added some more extra_credit. Added an alert message and changed the generateBtn click method.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Password-Generator
+# Password-Generator README
+
+This website integrates Bootstrap and semantic html to deliver an online personal profile that is both web accessible and responsive. It includes a navbar, sticky footer, and responsive images. Using Bootstrap's grid system, there is little need to use media queries as the basic functionality is adaptable for various devices and screen sizes. 
+
+Link to Password Generator Website: https://nicklynnjohnson.github.io/Password-Generator/
+
+![Password-Generator](./screenshot.png?raw=true "Password-Generator")
--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 // Assignment Code
+
+
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+alert("Thank you for coming to our website. To generate your password, you will need to answer several password criteria prompts. Please select the Generate Password button bellow and get ready :)");
+
+generateBtn.addEventListener("click", function(){
+  
+
+})
+
+
+
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN prompted for character types to include in the password
+//THEN I choose lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+
+
+
+
+
 
 // Write password to the #password input
-function writePassword() {
+/*function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+*/
